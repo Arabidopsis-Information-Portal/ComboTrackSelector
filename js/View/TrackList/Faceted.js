cd124b373accbfb3bcba4b74ff460265368e16c9
Vivek Krishnakumar <vivekkrishnakumar@gmail.com>
2014-12-22 14:29:38 -0500

    + Refactored ComboTrackSelector View to inherit the Faceted selector and extend renderInitial() method + Fixed issue of Faceted tracks not persisting between reloads + Upgraded base to JBrowse 1.11.5

---
+++ js/View/TrackList/Faceted.js
@@ -0,0 +1,146 @@
+define(
+    [
+        'dojo/_base/declare',
+        'dojo/_base/lang',
+        'dijit/layout/ContentPane',
+        'JBrowse/Util',
+        'JBrowse/View/TrackList/Faceted'
+    ],
+    function (
+        declare,
+        lang,
+        ContentPane,
+        Util
+    ) {
+
+var dojof = Util.dojof;
+return declare( 'ComboTrackSelector.View.TrackList.Faceted', JBrowse.View.TrackList.Faceted,
+   /**
+    * @lends JBrowse.View.TrackList.Faceted.prototype
+    */
+   {
+
+    renderInitial: function() {
+        this.containerElem = dojo.create( 'div', {
+            id: 'faceted_tracksel',
+            style: {
+                left: '-95%',
+                width: '95%',
+                zIndex: 500
+            }
+        },
+        document.body );
+
+        // make the tab that turns the selector on and off
+        dojo.create('div',
+            {
+                className: 'faceted_track_header',
+                innerHTML: '<div>Tracks Available in Faceted List</div>'
+            },
+            this.containerElem
+        );
+
+        var titles = this.config.title;
+        var title_list = titles.join("<br>");
+
+        dojo.create('div',
+            {
+                className: 'faceted_tracksel_on_off tab',
+                innerHTML: '</div><img src="' + this.browser.resolveUrl('plugins/ComboTrackSelector/img/faceted_icon.png') +
+                           '"><div>' + title_list + '</div><img src="' +
+                           this.browser.resolveUrl('plugins/ComboTrackSelector/img/green_right_arrow.png') + '">'
+            },
+            this.containerElem
+        );
+        this.mainContainer = new dijit.layout.BorderContainer(
+            { design: 'headline', gutters: false },
+            dojo.create('div',{ className: 'mainContainer' }, this.containerElem)
+        );
+
+
+        this.topPane = new dijit.layout.ContentPane(
+            { region: 'top',
+              id: "faceted_tracksel_top",
+              content: '<div class="title">Select Tracks</div> '
+                       + '<div class="topLink" style="cursor: help"><a title="Track selector help">Help</a></div>'
+            });
+        dojo.query('div.topLink a[title="Track selector help"]',this.topPane.domNode)
+            .forEach(function(helplink){
+                var helpdialog = new dijit.Dialog({
+                    "class": 'help_dialog',
+                    refocus: false,
+                    draggable: false,
+                    title: 'Track Selection',
+                    content: '<div class="main">'
+                             + '<p>The JBrowse Faceted Track Selector makes it easy to search through'
+                             + ' large numbers of available tracks to find exactly the ones you want.'
+                             + ' You can incrementally filter the track display to narrow it down to'
+                             + ' those your are interested in.  There are two types of filtering available,'
+                             + ' which can be used together:'
+                             + ' <b>filtering with data fields</b>, and free-form <b>filtering with text</b>.'
+                             + '</p>'
+                             + '  <dl><dt>Filtering with Data Fields</dt>'
+                             + '  <dd>The left column of the display contains the available <b>data fields</b>.  Click on the data field name to expand it, and then select one or more values for that field.  This narrows the search to display only tracks that have one of those values for that field.  You can do this for any number of fields.<dd>'
+                             + '  <dt>Filtering with Text</dt>'
+                             + '  <dd>Type text in the "Contains text" box to filter for tracks whose data contains that text.  If you type multiple words, tracks are filtered such that they must contain all of those words, in any order.  Placing "quotation marks" around the text filters for tracks that contain that phrase exactly.  All text matching is case insensitive.</dd>'
+                             + '  <dt>Activating Tracks</dt>'
+                             + "  <dd>To activate and deactivate a track, click its check-box in the left-most column.  When the box contains a check mark, the track is activated.  You can also turn whole groups of tracks on and off using the check-box in the table heading.</dd>"
+                             + "  </dl>"
+                             + "</div>"
+                 });
+                dojo.connect( helplink, 'onclick', this, function(evt) {helpdialog.show(); return false;});
+            },this);
+
+        this.mainContainer.addChild( this.topPane );
+
+        // make both buttons toggle this track selector
+        dojo.query( '.faceted_tracksel_on_off' )
+            .onclick( lang.hitch( this, 'toggle' ));
+
+        this.centerPane = new dijit.layout.BorderContainer({region: 'center', "class": 'gridPane', gutters: false});
+        this.mainContainer.addChild( this.centerPane );
+        var textFilterContainer = this.renderTextFilter();
+
+        this.busyIndicator = dojo.create(
+            'div', {
+                innerHTML: '<img src="'+this.browser.resolveUrl('img/spinner.gif')+'">',
+                className: 'busy_indicator'
+            }, this.containerElem );
+
+        this.centerPane.addChild(
+            new dijit.layout.ContentPane(
+                { region: 'top',
+                  "class": 'gridControls',
+                  content: [
+                      dojo.create( 'button', {
+                                       className: 'faceted_tracksel_on_off',
+                                       innerHTML: '<img src="'+this.browser.resolveUrl('img/left_arrow.png')+'"> <div>Back to browser</div>',
+                                       onclick: lang.hitch( this, 'hide' )
+                                   }
+                                 ),
+                      dojo.create( 'button', {
+                                       className: 'clear_filters',
+                                       innerHTML:'<img src="'+this.browser.resolveUrl('img/red_x.png')+'">'
+                                                 + '<div>Clear All Filters</div>',
+                                       onclick: lang.hitch( this, function(evt) {
+                                           this._clearTextFilterControl();
+                                           this._clearAllFacetControls();
+                                           this._async( function() {
+                                               this.updateQuery();
+                                               this._updateFacetCounts();
+                                           },this).call();
+                                       })
+                                   }
+                                 ),
+                      this.busyIndicator,
+                      textFilterContainer,
+                      dojo.create('div', { className: 'matching_record_count' })
+                  ]
+                }
+            )
+        );
+
+
+    }
+});
+});
---
+++ js/main.js
@@ -0,0 +1,225 @@
+define([
+           'dojo/_base/declare',
+           'dojo/_base/lang',
+           'dojo/on',
+           'dojo/keys',
+           'dojo/Deferred',
+           'dojo/DeferredList',
+           'dojo/topic',
+           'dojo/aspect',
+           'dojo/request',
+           'dojo/_base/array',
+           'JBrowse/Plugin',
+           'JBrowse/ConfigManager',
+           'dojo/domReady!'
+       ],
+
+       function(
+	   declare,
+	   lang,
+           on,
+           keys,
+           Deferred,
+           DeferredList,
+           topic,
+           aspect,
+           request,
+           array,
+	   JBrowsePlugin,
+	   ConfigManager
+       ) {
+
+
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+	console.log("Loaded ComboTrackSelector plugin");
+	var thisB = this;
+
+	//Load additional track config
+	var c = new ConfigManager({ bootConfig: thisB.config2, defaults: thisB._configDefaults(), browser: thisB });
+	c.getFinalConfig()
+	.then( dojo.hitch(thisB, function( finishedConfig2 ) {
+	    thisB.config2 = finishedConfig2;
+
+	}));
+
+         this.browser.afterMilestone( 'loadRefSeqs', dojo.hitch( this, function() {
+
+		 thisB.createNewTrackList(thisB.config2).then( lang.hitch( this, function() {
+
+		 thisB.initTrackMetadata( thisB.config2 );
+		 thisB._addTrackConfigs( thisB.config2 );
+
+		 this.browser.config.stores = dojo.mixin(this.browser.config.stores, thisB.config2.stores);
+         this.browser.config.tracks = dojo.mixin(this.browser.config.tracks, thisB.config2.tracks);
+         this.browser.trackConfigsByName = dojo.mixin(this.browser.trackConfigsByName, thisB.trackConfigsByName);
+
+		 this.browser.containerWidget.startup();
+		 this.browser.onResize();
+
+    		 // make our global keyboard shortcut handler
+		 //on( document.body, 'keypress', dojo.hitch( this, 'globalKeyHandler' ));
+
+		}));
+
+            }));
+
+    },
+
+
+/**
+ * Asynchronously create the track list.
+ * @private
+ */
+createNewTrackList: function(newconfig) {
+
+    var thisB = this;
+    newconfig = lang.clone( newconfig );
+
+    return this.browser._milestoneFunction('createTrack', function( deferred ) {
+        // find the tracklist class to use
+        var tl_class = !this.config.show_tracklist ? 'Null' : 'Faceted';
+
+        if( ! /\//.test( tl_class ) )
+            tl_class = 'ComboTrackSelector/View/TrackList/'+tl_class;
+
+        // load all the classes we need
+        require( [ tl_class ],
+                 lang.hitch( this, function( trackListClass ) {
+                     // instantiate the tracklist and the track metadata object
+                     this.trackListView = new trackListClass(
+                         lang.mixin(
+                             lang.clone( newconfig.trackSelector ) || {},
+                             {
+                                 trackConfigs: newconfig.tracks,
+                                 browser: this,
+                                 trackMetaData: this.trackMetaDataStore
+                             }
+                         )
+                     );
+
+                     // listen for track-visibility-changing messages from
+                     // views and update our tracks cookie
+                     this.subscribe( '/jbrowse/v1/n/tracks/visibleChanged', dojo.hitch( this, function() {
+                         this.cookie( "tracks",
+                                      this.view.visibleTrackNames().join(','),
+                                      {expires: 60});
+                     }));
+
+                     deferred.resolve({ success: true });
+        }));
+    });
+},
+
+_configDefaults: function() {
+
+    return {
+        tracks: [],
+
+        containerID: 'GenomeBrowser',
+        dataRoot: 'data',
+        show_tracklist: true,
+        show_nav: true,
+        show_overview: true,
+
+        refSeqs: "{dataRoot}/seq/refSeqs.json",
+        include: [
+	    'data/json/arabidopsis/trackList2.json'
+        ],
+        nameUrl: "{dataRoot}/names/root.json",
+
+        datasets: {
+            _DEFAULT_EXAMPLES: true,
+            volvox:    { url: '?data=sample_data/json/volvox',    name: 'Volvox Example'    },
+            modencode: { url: '?data=sample_data/json/modencode', name: 'MODEncode Example' },
+            yeast:     { url: '?data=sample_data/json/yeast',     name: 'Yeast Example'     }
+        },
+
+        highlightSearchedRegions: false,
+        highResolutionMode: 'disabled'
+    };
+},
+
+/**
+ * Add new track configurations.
+ * @private
+ */
+_addTrackConfigs: function( newconfig ) {
+
+    newconfig = lang.clone( newconfig );
+    var configs = newconfig.tracks || [];
+
+    if( ! newconfig.tracks )
+        newconfig.tracks = [];
+    if( ! this.trackConfigsByName )
+        this.trackConfigsByName = {};
+
+    array.forEach( configs, function(conf){
+
+        // if( this.trackConfigsByName[ conf.label ] ) {
+        //     console.warn("track with label "+conf.label+" already exists, skipping");
+        //     return;
+        // }
+
+        this.trackConfigsByName[conf.label] = conf;
+        newconfig.tracks.push( conf );
+
+    },this);
+
+    return configs;
+},
+
+/**
+ * Asynchronously initialize our track metadata.
+ */
+initTrackMetadata: function( newconfig ) {
+
+    newconfig = lang.clone( newconfig );
+
+    return this.browser._milestoneFunction( 'initTrackMetadata', function( deferred ) {
+
+        var metaDataSourceClasses = dojo.map(
+                                    (newconfig.trackMetadata||{}).sources || [],
+                                    function( sourceDef ) {
+                                        var url  = sourceDef.url || 'trackMeta.csv';
+                                        var type = sourceDef.type || (
+                                                /\.csv$/i.test(url)     ? 'csv'  :
+                                                /\.js(on)?$/i.test(url) ? 'json' :
+                                                'csv'
+                                        );
+                                        var storeClass = sourceDef['class']
+                                            || { csv: 'dojox/data/CsvStore', json: 'dojox/data/JsonRestStore' }[type];
+                                        if( !storeClass ) {
+                                            console.error( "No store class found for type '"
+                                                           +type+"', cannot load track metadata from URL "+url);
+                                            return null;
+                                        }
+                                        return { class_: storeClass, url: url };
+                                    });
+
+        require( Array.prototype.concat.apply( ['JBrowse/Store/TrackMetaData'],
+                                               dojo.map( metaDataSourceClasses, function(c) { return c.class_; } ) ),
+                 dojo.hitch(this,function( MetaDataStore ) {
+                     var mdStores = [];
+                     for( var i = 1; i<arguments.length; i++ ) {
+                         mdStores.push( new (arguments[i])({url: metaDataSourceClasses[i-1].url}) );
+                     }
+
+                     this.trackMetaDataStore =  new MetaDataStore(
+                         dojo.mixin( dojo.clone(newconfig.trackMetadata || {}), {
+                                         trackConfigs: newconfig.tracks,
+                                         browser: this,
+                                         metadataStores: mdStores
+                                     })
+                     );
+
+                     deferred.resolve({success:true});
+        }));
+    });
+},
+
+
+
+});
+});
