4688d0e93a2877cd663ca248753c7e1742f16a81
Vivek Krishnakumar <vkrishna@jcvi.org>
2015-06-24 14:32:08 -0400

    + Extend JBrowse/View/TrackList/Null to ComboTrackSelector plugin

---
+++ js/View/TrackList/Null.js
@@ -0,0 +1,19 @@
+define(
+        [
+            'dojo/_base/declare',
+            'JBrowse/View/TrackList/Null'
+        ],
+        function(
+            declare,
+            Null
+        ) {
+
+return declare( [ Null ],
+
+/**
+ * @lends JBrowse.View.TrackList.Null.prototype
+ */
+{
+
+});
+});
